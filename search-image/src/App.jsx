0d78423959b4e9a3362a88f93a7d0eb01c56fe11
สุวิทย์ ลีเจ้ยวาระ <139098129+Suwit1100@users.noreply.github.com>
2024-05-19 14:37:55 +0700

    state search

--- search-image/src/App.jsx
+++ search-image/src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import './App.css'
+import { FaSearch } from "react-icons/fa";
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [dataimage,setDataimage]=useState('');
 
   return (
     <div className="container">
@@ -10,9 +13,9 @@ function App() {
         </div>
         <div class="col-10">
           <div class="input-group input-group-lg">
-            <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" name='search' placeholder='กรองข้อความเพื่อค้นหาข้อมูล' />
-            <span class="input-group-text" id="inputGroup-sizing-lg">
-              <i class="fa-solid fa-magnifying-glass"></i>
+            <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" name='search' placeholder='กรองข้อความเพื่อค้นหาข้อมูล' value={search} onChange={(e)=>setSearch(e.target.value)} />
+            <span class="input-group-text" id="inputGroup-sizing-lg" role='button' >
+              <FaSearch />
             </span>
           </div>
         </div>
