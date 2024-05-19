29d2496af329e24687e09453c5680acb910d52d7
สุวิทย์ ลีเจ้ยวาระ <139098129+Suwit1100@users.noreply.github.com>
2024-05-19 16:29:10 +0700

    ค้นหารูปภาพ fetch api

--- search-image/src/App.jsx
+++ search-image/src/App.jsx
@@ -3,24 +3,54 @@ import './App.css'
 import { FaSearch } from "react-icons/fa";
 
 function App() {
-  const [search, setSearch] = useState('');
-  const [dataimage,setDataimage]=useState('');
+  const [word, setWord] = useState('');
+  const [dataimage, setDataimage] = useState([]);
+
+  function clicksearch() {
+    if (!word) {
+      alert("กรุณากรอกข้อความ");
+    } else {
+      const client_id = "YguB8p72O9UaAD2bhLhHcdWotiORcSwdE13fCfw2_HQ";
+      fetch(`https://api.unsplash.com/search/photos?client_id=${client_id}&query=${word}&per_page=12`)
+        .then(response => response.json())
+        .then(data => {
+          // ทำสิ่งที่ต้องการกับข้อมูลที่ได้
+          console.log(data.results);
+          const result = data.results;
+          if (result.length === 0) {
+            alert("ไม่พบข้อมูลรูปภาพ! กรุณาลองคำใหม่")
+          }
+          setDataimage(result)
+        })
+        .catch(error => {
+          console.error('เกิดข้อผิดพลาดในการดึงข้อมูล:', error);
+        });
+    }
+  }
 
   return (
     <div className="container">
-      <div class="row mt-5">
-        <div class="col">
+      <div className="row mt-5">
+        <div className="col-12 ">
+          ระบบค้นหารูปภาพด้วย API
         </div>
-        <div class="col-10">
-          <div class="input-group input-group-lg">
-            <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" name='search' placeholder='กรองข้อความเพื่อค้นหาข้อมูล' value={search} onChange={(e)=>setSearch(e.target.value)} />
-            <span class="input-group-text" id="inputGroup-sizing-lg" role='button' >
+        <div className="col-12">
+          <div className="input-group input-group-lg">
+            <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" name='search' placeholder='กรองข้อความเพื่อค้นหาข้อมูล' value={word} onChange={(e) => setWord(e.target.value)} />
+            <span className="input-group-text" id="inputGroup-sizing-lg" role='button' onClick={clicksearch}>
               <FaSearch />
             </span>
           </div>
         </div>
-        <div class="col">
-        </div>
+      </div>
+      <div className="row mt-2">
+        {dataimage.map((e) => {
+          return (
+            <div className="col-4 my-2" key={e.id}>
+              <img src={e.urls.full} alt="" />
+            </div>
+          );
+        })}
       </div>
     </div>
   )
